88c5dacc58eae66838762be9a01f33b5f256065c
mohuishou <1@lailin.xyz>
2016-09-04 21:10:03 +0800

    【新增】生成ics课程表

--- app/Http/Controllers/Jwc/ScheduleController.php
+++ app/Http/Controllers/Jwc/ScheduleController.php
@@ -32,7 +32,86 @@ class ScheduleController extends JwcBaseController{
         return $this->success('课程信息获取成功',$data);
     }
 
+    /**
+     * 生成ics文件
+     * @author mohuishou<1@lailin.xyz>
+     */
     public function ics(){
+
+        //获取课程表数据
+        $term=$this->getTerm();
+        $data=Schedule::where('term',$term)->get();
+
+        if(empty($data))
+            return $this->error(['error'=>'暂时没有课程数据']);
+
+        $ics=<<<EOD
+BEGIN:VCALENDAR
+PRODID:-//Google Inc//Google Calendar 70.9054//EN
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+X-WR-CALNAME:SCUPLUS-课表
+X-WR-TIMEZONE:Asia/Shanghai
+BEGIN:VTIMEZONE
+TZID:Asia/Shanghai
+X-LIC-LOCATION:Asia/Shanghai
+BEGIN:STANDARD
+TZOFFSETFROM:+0800
+TZOFFSETTO:+0800
+TZNAME:CST
+DTSTART:19700101T000000
+END:STANDARD
+END:VTIMEZONE
+EOD;
+
+        foreach ($data as $k => &$v){
+            $course=$v->course;
+            $v->course->teacher;
+
+            //构造教师名字
+            $teachers=$course->teacher;
+            $teacher="";
+            foreach ($teachers as $val){
+                $teacher.=$val->name . ",";
+            }
+            $address=$course->campus ."-". $course->building ."-". $course->classroom;
+
+            $week_start = substr($course -> allWeek, 0, 1 );
+            $week_end = substr($course -> allWeek,0,-1);
+            $week_arr=explode(',',$course->allWeek);
+
+            //重复周次
+            $count='COUNT='.count($week_arr);
+
+            //间隔周次
+            ($week_arr[1]-$week_arr[0])!=1 || $interval='INTERVAL=' . $week_arr[1]-$week_arr[0];
+
+            //整理上课节次
+            $session_start = substr($course->session, 0, 1 );
+            $session_end = substr($course->session,0,-1);
+            $attendClassTime=['0815','0910','1015','1110','1350','1445','1550','1645','1740','1920','2015','2110'];
+            $finishClassTime=['0900','0955','1100','1155','1435','1530','1635','1730','1825','2005','2100','2155'];
+
+            $school_start=strtotime("2016-09-05");//设置一学期当中的第一天
+            $course_start=$school_start+(($course->day)+($week_start-1)*7)*3600*24;
+            $course_start=date('Ymd',$course_start);
+            //按照ics时间整理
+            $dtstart=$course_start.'T'.$attendClassTime[$session_start-1].'00';
+            $dtend=$course_start.'T'.$finishClassTime[$session_end-1].'00';
+
+            $uid=md5($this->_user->id)."@scuplus.cn";
+
+            $description="课程号：".$course->courseId . "课序号：".$course->lessonId . " \r\n 学分： ". $course->credit . "周次： ". $week_start ."-".$week_end ."\r\n 平均成绩：" . $course->avg_grade ." 平均得分：" . $course->avg_star ;
+            $course->pass_rate || $fail_rate="无";
+            $fail_rate=(1-$course->pass_rate)*100;
+            $description .= "挂科率：". $fail_rate ."%";
+
+
+        }
+
+
+
 
     }
 
