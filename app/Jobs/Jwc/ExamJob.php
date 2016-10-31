107df1973a1de47786d3af456541e7c078751a3b
mohuishou <1@lailin.xyz>
2016-10-31 16:33:05 +0800

    完善教务处更新队列

--- app/Jobs/Jwc/ExamJob.php
+++ app/Jobs/Jwc/ExamJob.php
@@ -2,17 +2,23 @@
 
 namespace App\Jobs\Jwc;
 
+use App\Http\Controllers\Jwc\ExamController;
+use App\Jobs\Message\MessageJob;
+use App\Model\User;
 
+/**
+ * 成绩更新队列
+ * Class ExamJob
+ * @package App\Jobs\Jwc
+ */
 class ExamJob extends BaseJob
 {
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function __construct(User $user, $is_notify, $first)
     {
-        //
+        parent::__construct($user, $is_notify, $first);
+        if ($is_notify){
+            $this->_template_name="exam";
+        }
     }
 
     /**
@@ -20,8 +26,11 @@ class ExamJob extends BaseJob
      *
      * @return void
      */
-    public function handle()
+    public function handle(ExamController $exam)
     {
-        //
+        $res=$exam->updateBase($this->_user);
+        if($this->_is_notify && $res["status"]==1){
+            dispatch(new MessageJob($this->_user,$this->_template_name,$res["data"],$this->_first));
+        }
     }
 }
--- app/Jobs/Jwc/GradeJob.php
+++ app/Jobs/Jwc/GradeJob.php
@@ -3,25 +3,28 @@
 namespace App\Jobs\Jwc;
 
 
+use App\Http\Controllers\Jwc\GradeController;
+use App\Jobs\Message\MessageJob;
+use App\Model\User;
+
 class GradeJob extends BaseJob
 {
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function __construct(User $user, $is_notify, $first)
     {
-        //
+        parent::__construct($user, $is_notify, $first);
+        if ($is_notify){
+            $this->_template_name="grade";
+        }
     }
 
     /**
-     * Execute the job.
-     *
-     * @return void
+     * @param GradeController $grade
      */
-    public function handle()
+    public function handle(GradeController $grade)
     {
-        //
+        $res=$grade->updateBase($this->_user);
+        if($this->_is_notify && $res["status"]==1){
+            dispatch(new MessageJob($this->_user,$this->_template_name,$res["data"],$this->_first));
+        }
     }
 }
