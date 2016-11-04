0bc6c88e354a2e978450433913fb4a88daa91bbb
mohuishou <1@lailin.xyz>
2016-11-04 21:08:03 +0800

    删除课程表更新通知

--- app/Jobs/Jwc/ScheduleJob.php
+++ app/Jobs/Jwc/ScheduleJob.php
@@ -30,12 +30,12 @@ class ScheduleJob extends BaseJob
     public function handle(ScheduleController $schedule)
     {
         $res=$schedule->updateBase($this->_user);
-        if($this->_is_notify && $res["status"]==1 && $res["count"]>0){
-            //判断用户是否开启通知
-            if($this->_user->userNotify->jwc_schedule==1){
-                $message_job=(new MessageJob($this->_user,$this->_template_name,$res["data"]))->onQueue("message");
-                dispatch($message_job);
-            }
-        }
+//        if($this->_is_notify && $res["status"]==1 && $res["count"]>0){
+//            //判断用户是否开启通知
+//            if($this->_user->userNotify->jwc_schedule==1){
+//                $message_job=(new MessageJob($this->_user,$this->_template_name,$res["data"]))->onQueue("message");
+//                dispatch($message_job);
+//            }
+//        }
     }
 }
