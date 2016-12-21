04ed6d2b579efb30ad8e3170ec60085a1151be19
mohuishou <1@lailin.xyz>
2016-12-21 16:13:19 +0800

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,9 @@
 
 namespace App\Console;
 
+use App\Http\Controllers\Jwc\EvaluateController;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Log;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -24,6 +26,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        $schedule->call(function (){
+            $eva_update=new EvaluateController();
+            $eva_update->reEvaluate();
+            Log::info("评教复查开始！");
+        })->everyThirtyMinutes();
     }
 }
